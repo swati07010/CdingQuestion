c1e74f57c66f0f06e0e3fd0d7df1a62ec8e2c3dc
swati07010 <105536133+swati07010@users.noreply.github.com>
2022-05-19 20:42:50 +0530

    check if a number is a perfect square

---
+++ perfectSquare.js
@@ -0,0 +1,10 @@
+//Check if number is perfect Square
+let n=prompt("enter the number");
+
+    let b=Math.sqrt(n);
+    if(n%b==0){
+        console.log("Number is perfect Square");
+        
+    }else{
+        console.log("number is not perfect Square");
+    }
