e0480d5b677f607002a9bd9e7b4ec359c78826b8
swati07010 <105536133+swati07010@users.noreply.github.com>
2022-06-24 16:25:23 +0530

    Minimum Difference

---
+++ Minimum Difference.js
@@ -0,0 +1,16 @@
+function MinimumDifference(arr){
+    let n=arr.length;
+    let temp=0;
+    for(let i=0;i<n;i++){
+        for(let  j=n-1;j>0;j--){
+          if(j>i){
+            if(arr[j]-arr[i]<temp){
+              temp=arr[j]-arr[i];
+            }
+          }
+        }
+    }
+    console.log(temp);
+  }
+  let arr=[20,3,10,6,4,8,1];
+  MinimumDifference(arr);
